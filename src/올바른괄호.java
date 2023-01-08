bada7f04004f103ebe478e7e24df58af0ab279b9
rdyjun <rdyjun00@gmail.com>
2023-01-08 21:25:21 +0900

    올바른 괄호 시작

--- src/올바른괄호.java
+++ src/올바른괄호.java
@@ -1,23 +1,28 @@
-//import java.util.Stack;
-//
-//public class 올바른괄호 {
-//    public static boolean main(String[] args) {
-//        String s = "(())()";
-//
-//        char c = s.charAt(0);
-//
-//        if (c == ')' || s.length() % 2 == 1 || s.charAt(s.length() - 1) == '(')
-//            return false;
-//
-//        Stack<Character> a = new Stack<>();
-//        a.push('(');
-//
-//        for (int i = 1; i < a.size(); i++) {
-//            if(a.get(i - 1) == '(' && s.charAt(i) == ')'){
-//
-//            }
-//        }
-//
-//        System.out.println(a.replaceAll("\\(.*\\)", ""));
-//    }
-//}
+import java.util.Stack;
+
+public class 올바른괄호 {
+    public static void main(String[] args) {
+        String s = "()()";
+
+        int[] arr = new int[s.length()];
+        int a, b;  // (와 )의 위치
+
+        //arr에 s배열을 숫자로 넣기
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '(')
+                arr[i] = 0;
+            else
+                arr[i] = 1;
+        }
+
+        if (s.charAt(0) == ')' || s.length() % 2 == 1 || s.charAt(s.length() - 1) == '(')
+            System.out.println("false");
+
+        for (int i = 0; i < s.length(); i++){
+
+        }
+
+
+        System.out.println("true");
+    }   // ((( )( )))
+}
