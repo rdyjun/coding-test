6c073acb93ee3d6c8ffcc5f3bdd58989b3c23943
RDYJ <45596014+rdyjun@users.noreply.github.com>
2023-02-11 12:48:48 +0900

    전체 코드 업로드 -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write("Case #" + (i + 1) + ": " + a + " + " +  b + " = " + (a + b) + "\n");;
+        }
+        bw.close();
+    }
+}
---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 0; i < n; i++){
+            for(int o = 0; o < n - i - 1; o++)
+                bw.write(" ");
+            for(int k = 0; k <= i; k++)
+                bw.write("*");;
+            bw.write("\n");
+        }
+        bw.close();
+    }
+}
