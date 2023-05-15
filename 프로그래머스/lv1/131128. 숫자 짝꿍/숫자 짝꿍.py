5cb47cd9d9d2e0369f52cf5dd4ccf0bd3589ee9c
RDYJ <45596014+rdyjun@users.noreply.github.com>
2023-05-15 15:01:13 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,7 +1,5 @@
 from collections import Counter
 
 def solution(X, Y):
-    if len([i for i in range(0, 10) if min(X.count(str(i)), Y.count(str(i))) != 0]) <= 0:
-        return "-1"
-    st = ''.join([str(i) * min(X.count(str(i)), Y.count(str(i))) for i in range(9, -1, -1) if min(X.count(str(i)), Y.count(str(i)))]).lstrip('0')
-    return st if st != "" else "0"
+    answer = ''.join([str(i) * min(X.count(str(i)), Y.count(str(i))) for i in range(9, -1, -1)])
+    return '-1' if answer == '' else '0' if answer.lstrip("0") == '' else answer.lstrip("0")
