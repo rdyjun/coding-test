66c63ddb1bf51b2bf37a5a6e6d4725be5682aecf
RDYJ <45596014+rdyjun@users.noreply.github.com>
2023-07-11 09:57:21 +0900

    [level 1] Title: 소수 찾기, Time: 318.51 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -1,12 +1,6 @@
-import math
-
 def solution(n):
-    arr = [i for i in range(2, n + 1)]
-    m = 1
-    while m <= math.sqrt(n):
-        m += 1
-        if arr.count(m) > 0:
-            arr = arr[:arr.index(m) + 1] + [k for k in arr[arr.index(m) + 1:] if k % m != 0 or k / m == 1]
-            continue
-        
+    arr = set(range(2, n + 1))
+    for i in range(2, n + 1):
+        if i in arr:
+            arr -= set(range(i * 2, n + 1, i))
     return len(arr)
