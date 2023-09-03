a887494622eeab2787983d658cf97a32ccd36f78
rdyj <rdyjun00@gmail.com>
2023-09-03 11:57:25 +0900

    [unrated] Title: x 사이의 개수, Time: 7.45 ms, Memory: 91 MB -BaekjoonHub

--- 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.java
+++ 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.java
@@ -2,11 +2,8 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String myString) {
-        myString = myString + "a";
-        int[] answer = Arrays.stream(myString.split("x"))
+        return Arrays.stream(myString.split("x", myString.length()))
             .mapToInt(v -> v.length())
             .toArray();
-        answer[answer.length - 1]--;
-        return answer;
     }
 }
