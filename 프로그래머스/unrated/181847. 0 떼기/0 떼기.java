7c1ec87aa381338e64264b08e25b593a55b12547
rdyj <rdyjun00@gmail.com>
2023-09-03 11:29:54 +0900

    [unrated] Title: 0 떼기, Time: 0.03 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -1,10 +1,5 @@
 class Solution {
     public String solution(String n_str) {
-        int n;
-        for (n = 0; n < n_str.length(); n++)
-            if (n_str.charAt(n) != '0')
-                break;
-        
-        return n_str.substring(n, n_str.length());
+        return String.valueOf(Integer.parseInt(n_str));
     }
 }
