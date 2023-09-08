3665a1dfc0a220396fbac648ff44962ba081ed6a
rdyj <rdyjun00@gmail.com>
2023-09-08 11:04:58 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 41.21 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/lv4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.java
+++ 프로그래머스/lv4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.java
@@ -1,18 +1,17 @@
 class Solution {
-    private int answer = 0;
-    
-    private void dfs (int open, int close, int n) {
+    private int dfs (int open, int close, int n) {
+        int answer = 0;
         if (n == open && n == close)
-            this.answer++;
+            return 1;
         
         if (n - open > 0)
-            dfs(open + 1, close, n);
+            answer += dfs(open + 1, close, n);
         if (n - close > 0 && n - open <= n - close)
-            dfs(open, close + 1, n);
+            answer += dfs(open, close + 1, n);
+        return answer;
     }
     
     public int solution(int n) {
-        dfs(1, 1, n);
-        return this.answer;
+        return dfs(1, 1, n);
     }
 }
