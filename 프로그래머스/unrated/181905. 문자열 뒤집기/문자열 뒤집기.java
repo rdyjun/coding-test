c83a631252dc56c8d2d6ff578ffb993960543ac6
rdyj <rdyjun00@gmail.com>
2023-10-16 10:45:38 +0900

    [level unrated] Title: 문자열 뒤집기, Time: 12.06 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        return my_string.substring(0, s)
+            + new StringBuffer(my_string.substring(s, e + 1)).reverse().toString()
+            + my_string.substring(e + 1);
+    }
+}
