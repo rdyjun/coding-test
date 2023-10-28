6fcbf73c40565265fec95f475ce076e55553a485
rdyj <rdyjun00@gmail.com>
2023-10-28 11:45:34 +0900

    [level 2] Title: 땅따먹기, Time: 53.72 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,23 +1,30 @@
 import java.util.*;
 
 class Solution {
+    private int col;
+    private int row;
+    
     private int[] getTwoMax (int[] values) {
+        int valuesLength = values.length;
         Arrays.sort(values);
-        return new int[]{values[values.length - 1], values[values.length - 2]};
+        return new int[]{values[valuesLength - 1], values[valuesLength - 2]};
+    }
+    private void setMaxValues (int[][] land, int[] maxValues, int row) {
+        for (int index = 0; index < this.col; index++) {
+            if (maxValues[0] == land[row][index]) {
+                land[row + 1][index] += maxValues[1];
+                continue;
+            }
+            land[row + 1][index] += maxValues[0];
+        }
     }
     public int solution(int[][] land) {
-        int col = land[0].length;
-        int row = land.length;
+        this.col = land[0].length;
+        this.row = land.length;
         
         for (int i = 0; i < row - 1; i++) {
-            int[] max = getTwoMax(land[i].clone());
-            for (int k = 0; k < col; k++) {
-                if (max[0] == land[i][k]) {
-                    land[i + 1][k] += max[1];
-                    continue;
-                }
-                land[i + 1][k] += max[0];
-            }
+            int[] maxValues = getTwoMax(land[i].clone());
+            setMaxValues(land, maxValues, i);
         }
         return getTwoMax(land[row - 1])[0];
     }
