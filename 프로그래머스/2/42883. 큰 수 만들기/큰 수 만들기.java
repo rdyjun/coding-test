da996374b9227827afdafd86b14512a08f8252f0
rdyj <rdyjun00@gmail.com>
2023-12-26 17:25:58 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.15 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+class Solution {
+    public String solution(String number, int k) {
+        StringBuilder stringBuilder = new StringBuilder();
+        Stack<Integer> stk = new Stack<>();
+        int len = number.length();
+        for (int i = 0; i < len; i++) {
+            int n = number.charAt(i);
+            while(k > 0) {
+                if (stk.isEmpty() || stk.peek() >= n)
+                    break;
+                k--;
+                stk.pop();
+            }
+            stk.push(n);
+        }
+        while (!stk.isEmpty())
+            stringBuilder.append(stk.pop() - '0');
+        for (int i = 0; i < k; i++)
+            stringBuilder.deleteCharAt(i);
+        return stringBuilder
+                .reverse()
+                .toString();
+    }
+}
