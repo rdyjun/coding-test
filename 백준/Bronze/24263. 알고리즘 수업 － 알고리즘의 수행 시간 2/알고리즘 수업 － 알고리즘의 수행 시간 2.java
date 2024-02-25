1e4183f9346c40cadc5acad8507eb19947c93e60
rdyj <rdyjun00@gmail.com>
2024-02-25 15:36:13 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 140 ms, Memory: 15816 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        IO.init();
+
+        int n = Integer.parseInt(IO.read());
+        IO.write(n + "\n" + 1);
+
+        IO.close();
+    }
+}
+
+class IO {
+    private static BufferedReader br;
+    private static BufferedWriter bw;
+
+    public static void init() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+
+    public static void close() throws IOException {
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static String read() throws IOException {
+        return br.readLine();
+    }
+
+    public static void write(Object message) throws IOException {
+        bw.write(String.valueOf(message));
+    }
+}
