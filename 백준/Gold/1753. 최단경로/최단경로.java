579e2b87400e49e8116bc152ef11a80c3233f0a4
rdyj <rdyjun00@gmail.com>
2024-03-21 16:42:01 +0900

    [Gold IV] Title: 최단경로, Time: 768 ms, Memory: 115608 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -4,12 +4,8 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.PriorityQueue;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -19,6 +15,12 @@ public class Main {
     private static int edgeCount;
 
     private static int startVertices;
+
+    private static List<List<Node>> graph;
+
+    private static int[] distance;
+
+    private static PriorityQueue<Node> priorityQueue;
 
     public static void main(String[] args) {
         IO.init();
@@ -30,89 +32,76 @@ public class Main {
         StringTokenizer input = new StringTokenizer(IO.read(), " ");
         verticesCount = Integer.parseInt(input.nextToken());
         edgeCount = Integer.parseInt(input.nextToken());
-
         startVertices = Integer.parseInt(IO.read());
-        Map<Integer, List<Node>> graph = initializeGraph();
 
-        int[] answer = dijkstra(graph);
-        printAnswer(answer);
+        initializeGraph();
+        initializeDistance();
+
+        dijkstra();
+        printAnswer();
     }
 
-    private static void printAnswer(int[] answer) {
+    private static void printAnswer() {
         for (int index = 0; index < verticesCount; index++) {
-            if (answer[index] == Integer.MAX_VALUE) {
+            if (distance[index] == Integer.MAX_VALUE) {
                 IO.write("INF\n");
                 continue;
             }
 
-            IO.write(answer[index] + "\n");
+            IO.write(distance[index] + "\n");
         }
     }
 
-    private static int[] dijkstra(Map<Integer, List<Node>> graph) {
-        int[] nodeCost = new int[verticesCount];
-        Arrays.fill(nodeCost, Integer.MAX_VALUE);
-        nodeCost[startVertices - 1] = 0;
-        Set<Integer> visited = new HashSet<>();
+    private static void dijkstra() {
+        priorityQueue = new PriorityQueue<>();
+        priorityQueue.add(new Node(startVertices - 1, 0));
 
-        PriorityQueue<Node> pq = new PriorityQueue();
-        pq.add(new Node(startVertices, 0));
+        while (!priorityQueue.isEmpty()) {
+            Node now = priorityQueue.poll();
 
-        while (!pq.isEmpty()) {
-            Node now = pq.poll();
-
-            visited.add(now.getNode());
-
-            List<Node> nodes = graph.getOrDefault(now.getNode(), new ArrayList<>());
-            if (!nodes.isEmpty()) {
-                updateNodeCost(nodeCost[now.getNode() - 1], nodeCost, nodes, visited, pq);
-            }
+            updateNodeCost(now);
         }
-
-        return nodeCost;
     }
 
-    private static void updateNodeCost(int cost, int[] nodeCost, List<Node> nextNodeCost, Set<Integer> visited, PriorityQueue<Node> pq) {
-		for (Node node : nextNodeCost) {
-			if (visited.contains(
-                    node.getNode())) {
-				continue;
-			}
+    private static void updateNodeCost(Node now) {
+        List<Node> nextNodes = graph.get(now.getNode());
+        int weight = now.getWeight();
 
-			if (nodeCost[node.getNode() - 1] == Integer.MAX_VALUE
-                    || node.getWeight() + cost < nodeCost[node.getNode() - 1]) {
-                pq.add(new Node(node.getNode(), node.getWeight() + cost));
-				nodeCost[node.getNode() - 1] = node.getWeight() + cost;
+		for (Node node : nextNodes) {
+            int nodeNum = node.getNode();
+            int sum = node.getWeight() + weight;
+
+			if (distance[nodeNum] == Integer.MAX_VALUE
+                    || sum < distance[nodeNum]) {
+                priorityQueue.add(new Node(nodeNum, sum));
+                distance[nodeNum] = sum;
 			}
 		}
     }
 
-    private static Map<Integer, List<Node>> initializeGraph() {
-        Map<Integer, Map<Integer, Integer>> valueCheck = new HashMap<>();
-        Map<Integer, List<Node>> graph = new HashMap<>();
+    private static void initializeDistance() {
+        distance = new int[verticesCount];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[startVertices - 1] = 0;
+    }
+
+    private static void initializeGraph() {
+        graph = new ArrayList<>();
+        for (int i = 0; i < verticesCount; i++) {
+            graph.add(new ArrayList<>());
+        }
 
         while (edgeCount-- > 0) {
             StringTokenizer input = new StringTokenizer(IO.read(), " ");
-            int a = Integer.parseInt(input.nextToken());
-            int b = Integer.parseInt(input.nextToken());
+
+            int a = Integer.parseInt(input.nextToken()) - 1;
+            int b = Integer.parseInt(input.nextToken()) - 1;
             int w = Integer.parseInt(input.nextToken());
 
-            Map<Integer, Integer> vc = valueCheck.getOrDefault(a, new HashMap<>());
-            int weight = vc.getOrDefault(b, -1);
-
-            if (weight > -1 && weight <= w) {
-                continue;
-            }
-
-            vc.put(b, w);
-            valueCheck.put(a, vc);
-
-            List<Node> arr = graph.getOrDefault(a, new ArrayList<>());
+            List<Node> arr = graph.get(a);
             arr.add(new Node(b, w));
-            graph.put(a, arr);
+            graph.set(a, arr);
         }
-
-        return graph;
     }
 
 }
