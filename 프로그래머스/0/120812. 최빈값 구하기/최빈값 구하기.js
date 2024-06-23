495701856ac8d72dc24bb4fe7711971f6a4b61ac
rdyj <rdyjun00@gmail.com>
2024-06-23 21:55:15 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -1,20 +1,3 @@
-// function solution(array) {
-//     let map = new Map();
-    
-//     array.forEach(v => {
-//         map.set(v, (map.get(v) || 0) + 1);
-//     });
-    
-//     let max = Math.max(...map.values());
-//     let arr = [...map.entries()].filter(([key, value]) => value === max)
-    
-//     if (arr.length > 1) {
-//         return -1;
-//     }
-    
-//     return arr[0][0];
-// }
-
 function solution(array) {
     let map = new Map();
     
